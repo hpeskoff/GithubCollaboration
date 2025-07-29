17787a869c9c507d6a82e74fcec43a76dcb6e3d0
Joelle Rice <joellerice17@gmail.com>
2025-07-29 10:07:42 -0400

    evaluation backbones

---
+++ GithubCollaboration/InitialEvaluation.swift
@@ -0,0 +1,15 @@
+//
+//  InitialEvaluation.swift
+//  GithubCollaboration
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct InitialEvaluation: View {
+    var body: some View {
+        Text("About 💁🏼")
+    }
+}
