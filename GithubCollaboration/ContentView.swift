e232c4831e9fe61072ee06362f399452d70e639a
Joelle Rice <joellerice17@gmail.com>
2025-07-29 09:53:40 -0400

    added tag line

--- GithubCollaboration/ContentView.swift
+++ GithubCollaboration/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("ThriveCheck")
-                .font(.custom("Baby Doll", size: 24))
-                .foregroundColor(Color(red: 0.286, green: 0.431, blue: 0.425))
-            Image("logo")
-                .resizable()
-                .frame(width: 170, height: 170)
-                .padding(.leading, 0)
+        ZStack
+        {
+            Color(red: 1.0, green: 0.98, blue: 0.94)
+                .ignoresSafeArea()
+            VStack {
+                Text("ThriveCheck")
+                    .font(.custom("Baby Doll", size: 40))
+                    .foregroundColor(Color(red: 0.286, green: 0.431, blue: 0.425))
+                Image("logo_transparent")
+                    .resizable()
+                    .frame(width: 170, height: 170)
+                    .padding(.leading, 0)
+                Text("Your wellness is our happiness")
+                    .font(.custom("Baby Doll", size: 22))
+                    .foregroundColor(Color(hue: 0.085, saturation: 0.592, brightness: 0.509))
+                    .padding(.top, 10)
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
