6539e9dd36b8c1630dbe27f69cc91c4b9c642054
Olivia Bak <osb3825@gmail.com>
2025-07-29 11:26:23 -0400

    helping little old joelle

--- GithubCollaboration/Nutrition.swift
+++ GithubCollaboration/Nutrition.swift
@@ -7,10 +7,51 @@
 
 import Foundation
 import SwiftUI
-
 struct Nutrition: View {
+@State private var age = ""
+@State private var gender = ""
+@State private var active = ""
+@State private var textTitle = "input"
+@State private var results = ""
+    
+    func calculate(age_: String, gender_ : String, active_: String) -> String {
+        if (gender == "F") {
+            if(age <= "8" && age > "2")
+            {
+                return results
+            }
+        }
+        return results
+    }
+    
     var body: some View {
-        Text("nutriiotn olivia")
+        VStack {
+            Text("Nutrition")
+                .font(.custom("Baby Doll", size: 40))
+                .foregroundColor(Color(red: 0.286, green: 0.431, blue: 0.425))
+        
+            Text("please input your info")
+                .font(.custom("Baby Doll", size: 22))
+                .foregroundColor(Color(hue: 0.085, saturation: 0.592, brightness: 0.509))
+                .padding(.top, 10)
+         //   Spacer()
+            
+                VStack {
+            //        Text(textTitle)
+                  //  Text("")
+                    TextField(" Age", text: $age)
+                    TextField(" Gender (M/F)", text: $gender)
+                    TextField(" Activity Level (1-10)", text: $active)
+                    
+                    Button("calculate") {
+                        print(calculate(age_: age, gender_: gender, active_: active))
+                    }
+                    
+                    
+            }
+            
+            
+            
+        }
     }
 }
-
